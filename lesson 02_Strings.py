5376e584b25ad33bc71228815cfd66c5f6bceae5
Бахитжан Тулегенов <bahitzantulegenov@MacBook-Air-Bahitzan.local>
2025-07-03 06:59:41 +0500

    Add lesson 02Strings practice

---
+++ lesson 02_Strings.py
@@ -0,0 +1,22 @@
+#Lesson 02_Strings
+#Practiced creating strings, multiline strings, and printing them.
+#Date: 2025-07-02
+
+message = "Hello World!"
+print(message)
+name = "Tom"
+print(name)
+
+text = ("Laudate omnes gentes laudate "
+        "Magnificat in secula")
+print(text)
+
+text = '''Hello my dear!
+How are you?
+Whtat do you do?
+Do you study or working?'''
+print(text)
+
+text = ("Hello my dear! "
+        "How are you feeling today?")
+print(text)

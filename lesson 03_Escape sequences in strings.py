784d415741feef2b626b27f4a015a309d35566dc
Бахитжан Тулегенов <bahitzantulegenov@MacBook-Air-Bahitzan.local>
2025-07-03 08:45:56 +0500

    Add lesson 03_List practice

---
+++ lesson 03_Escape sequences in strings.py
@@ -0,0 +1,21 @@
+#Lesson 02_Escape sequences in strings
+#Practicing
+#Date: 2025-07-03
+
+text = "Message:\n\"Hello World!\""
+print(text)
+
+text = "She said: \"Hi!\""
+print(text)
+
+text = 'It\'s a test'
+print(text)
+
+path = r"C:\python\name.txt"
+print(path)
+
+text = "Line1\nLine2"
+print(text)
+
+
+

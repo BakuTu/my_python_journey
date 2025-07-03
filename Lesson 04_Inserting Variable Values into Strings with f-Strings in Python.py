d717fa727ee0236c6ab0e3314b8ae768d2c686f4
Бахитжан Тулегенов <bahitzantulegenov@MacBook-Air-Bahitzan.local>
2025-07-03 10:44:35 +0500

    Add lesson 04_Inserting Variable Values into Strings with f-Strings in Python.py

---
+++ Lesson 04_Inserting Variable Values into Strings with f-Strings in Python.py
@@ -0,0 +1,8 @@
+#Lessodn 04_Inserting Variable Values into Strings with f-Strings in Python
+#Practicing
+#Date: 2025-07-03
+
+userName = "Tom"    # create a variable with user's name
+userAge = 37    # create a variable with user's age
+user = f"name: {userName} age: {userAge}"   # creating a string with the variables inside {} using f - string
+print(user)     # print: name: Tom age 37

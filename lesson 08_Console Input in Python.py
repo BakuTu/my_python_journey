3c1570a4c8edf614506f4f4c304c23426ed4726e
Бахитжан Тулегенов <bahitzantulegenov@MacBook-Air-Bahitzan.local>
2025-07-04 17:03:24 +0500

    Add lesson 08_Console Input in Python

---
+++ lesson 08_Console Input in Python.py
@@ -0,0 +1,30 @@
+# Lesson 08_Console Input in Python
+# Practicing
+# Date: 2025-07-04
+
+name = input("Enter your name: ")   # Ask the user to enter their name and save it in 'name'
+print(f"Your name: {name} ")    # Print the name using an f-string
+
+name = input("Your name: ") # Ask for name
+age = input("Your age: ")    # Ask for age
+print(f"Name: {name} Age: {age} ")  # Print both values on one line
+
+#   Practicing
+
+color = input("What is your favorite color? ")
+print(f"Your favorite color is -  {color} ")
+
+name = input("What is your name? ")
+age = input("What is your age? ")
+print(f"Your name is: {name}, Your age is {age} ")
+
+number = input("Enter any number: ")
+print(type(number))
+
+number = input("Enter any number: ")
+number_int = int(number)
+print(number_int)
+print(type(number_int))
+
+
+

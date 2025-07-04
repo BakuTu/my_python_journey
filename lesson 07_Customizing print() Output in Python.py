aca06cdf08b4561f1b9c4c7d3a175779d3d93b1f
Бахитжан Тулегенов <bahitzantulegenov@MacBook-Air-Bahitzan.local>
2025-07-04 09:58:50 +0500

    Add lesson 07_List practice

---
+++ lesson 07_Customizing print() Output in Python.py
@@ -0,0 +1,31 @@
+# Lesson 06_Customizing print() Output in Python
+# Practicing
+# Date: 2025-07-04
+
+print("Hello World", end=" ")   # prints "Hello World", then adds a space instead of a new line
+print("Hello METANIT.COM", end=" ") #prints "Hello METANIT.COM", then adds a space
+print("Hello Python")   #prints "Hello Python", then adds a new line (default)
+
+print("Hello World", end=" and ")   # prints "Hello World", then " and "
+print("Hello METANIT.COM", end=" and ") # prints "Hello METANIT.COM", then " and "
+print("Hello Python")   #prints "Hello Python", then a new line
+
+print("I", end=" ")
+print("am", end=" ")
+print("learning", end=" ")
+print("Python", end=" ")
+print("step", end=" ")
+print("by", end=" ")
+print("step")
+
+print("Apple", end=" --> ")
+print("Banana", end=" --> ")
+print("Cherry")
+
+
+
+
+
+
+
+

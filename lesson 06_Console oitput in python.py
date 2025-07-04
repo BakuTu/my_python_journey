13b6b48fb5bfa3a02290603fe0501b92f5318ff5
Бахитжан Тулегенов <bahitzantulegenov@MacBook-Air-Bahitzan.local>
2025-07-04 07:07:43 +0500

    Add lesson 06_List practice

---
+++ lesson 06_Console oitput in python.py
@@ -0,0 +1,7 @@
+# Lesson 06_Console Output in Python
+# Practicing
+# Date: 2025-07-04
+
+print("Hello World")    # prints "Hello World" on a new line
+print("Hello METANIT.COM")  #prints "Hello METANIT.COM" on the next line
+print("Hello Python")   # prints "Hello Python" on another new line

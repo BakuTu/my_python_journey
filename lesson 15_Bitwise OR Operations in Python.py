1fa9f7c0f0cb519457501c0210d45539743ddb14
Бахитжан Тулегенов <bahitzantulegenov@MacBook-Air-Bahitzan.local>
2025-07-09 16:20:34 +0500

    Add lesson 15_List practice

---
+++ lesson 15_Bitwise OR Operations in Python.py
@@ -0,0 +1,16 @@
+# Lesson 15: Bitwise OR Operations in Python
+# Practicing
+# Date: 2025-07-09
+
+x1 = 2  # binary 010
+y1 = 5 # binary 101
+z1 = x1 | y1 # bitwise OR operation. Result in binary: 111; in decimal: 7
+print(f"z1 = {z1}") # z1 = 7
+print(f"z1 = {z1:0b}")  # z1 = 111
+
+
+x2 = 4  # binary 100
+y2 = 5 # binary 101
+z2 = x2 | y2 # bitwise OR operation. Result in binary: 101; in decimal: 5
+print(f"z2 = {z2}") # z2 = 5
+print(f"z2 = {z2:0b}")  # z2 = 101

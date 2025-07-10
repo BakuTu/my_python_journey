87cdc728023dcb622ee3c5c8dc053186a1c584b1
Бахитжан Тулегенов <bahitzantulegenov@MacBook-Air-Bahitzan.local>
2025-07-10 07:26:30 +0500

    Add lesson 18_List practice

---
+++ lesson 18_Bitwise Shift Operations in Python.py
@@ -0,0 +1,17 @@
+# Lesson 18: Bitwise Shift Operations in Python
+# Practicing
+# Date: 2025-07-10
+
+a = 16  # 10000
+b = 2
+c = a << b  # 1000000 = 64
+print(c)    # 64
+d = a >> b  # 100 = 4
+print(d)    # 4
+
+a = 22  # 10110
+b = 2
+c = a << b  # 1011000 = 88
+print(c)    # 88
+d = a >> b # 101 = 5
+print(d)    # 5
